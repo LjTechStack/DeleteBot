b8018d4cb4839e20a644db22174d547c883a0d0f
Lj <liangjiajiang@gmail.com>
2020-06-27 15:24:07 -0400

    added client_key to utils folder

--- delete_bot.py
+++ delete_bot.py
@@ -1,6 +1,7 @@
 import discord
 from datetime import datetime, timedelta
-
+from help_menu.help import help
+from helpers.main.utils import utils, keys
 client = discord.Client()
 
 @client.event
@@ -11,15 +12,23 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.author == client.user:
+        if utils.containsKeywords(message.content):
+            message.delete()
         return
+
+    action = "something to start with"
 
     if message.content.startswith('$delete'):
         command = message.content.split(" ")
-        if len(command) == 3:
+        if len(command) < 3:
             number_of_messages = await do_command(command[1:], message)
             action = "we have deleted: " + str(number_of_messages) + " messages"
+        elif command[1] == 'help':
+            help()
+            action = "helping"
+
         else:
-            action = "we could not process your request."
+            action = "To use $delete help"
 
         await message.channel.send(action)
 
@@ -29,21 +38,18 @@ async def do_command(command, message):
         return await start_delete_process(message)
 
 
-
 async def start_delete_process(message):
     count = 0
     yesterday = datetime.now() - timedelta(hours=12)
 
-    async for elem in message.channel.history(limit=1000):
+    async for elem in message.channel.history(limit=10000):
+        if yesterday < elem.created_at:
+            break
         if elem.author == message.author:
-            if yesterday < elem.created_at:
-                print(elem)
-                await elem.delete()
-                count += 1
-
-            else:
-                break
+            print(elem)
+            await elem.delete()
+            count += 1
     return count
 
 
-client.run('bot client_id')
+client.run(keys.CLIENT_KEY)
